921164692c28ed4ef8c92f2f6218692aee0fcb25
pawan495 <pawanpreetkaur495@gmail.com>
2022-03-14 19:21:51 -0400

    add other code in server

--- server.js
+++ server.js
@@ -1,3 +1,55 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const notes = require("./Develop/db/db.json");
+const { DH_CHECK_P_NOT_SAFE_PRIME, SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
+
+const app = express();
+var PORT = process.env.PORT || 3017;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// STATIC MIDDLEWARE
+app.use(express.static("./Develop/public"));
+
+
+
+//GET API db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Develop/db/db.json"))
+});
+
+// Post function to add new notes to db.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./Develop/db/db.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./Develop/db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+//Start listen
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
